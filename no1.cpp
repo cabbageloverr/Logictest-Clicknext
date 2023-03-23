5bb656b2b473f3ed9d73ad9bf3fdab433f170db1
Natthima Noinon <122480510+cabbageloverr@users.noreply.github.com>
2023-03-23 20:00:45 +0700

    Update no1.cpp

--- no1.cpp
+++ no1.cpp
@@ -1,29 +1,28 @@
-#include <iostream> 
+#include <iostream>
 
 using namespace std;
 
-int main(){
+int main()
+{
     int arr[5];
     int sum;
-    
-    
 
-    for (int i=0; i < 5; i++){
+    for (int i = 0; i < 5; i++)
+    {
         cin >> arr[i];
     }
     cin >> sum;
 
-    for (int i = 0; i < 5; i++){
-        for (int j = i+1; j < 5; j++){
-            if (arr[i] + arr[j] == sum){
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = i + 1; j < 5; j++)
+        {
+            if (arr[i] + arr[j] == sum)
+            {
                 cout << arr[i] << "," << arr[j] << endl;
             }
-            
         }
-        
-        
     }
-    
 
-    
+    return 0;
 }
